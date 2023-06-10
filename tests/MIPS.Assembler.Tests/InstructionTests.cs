db3b214f77107e1b8e8838756f526e21743264e7
Adam Dernis <Avid29@Live.com>
2023-06-10 18:13:29 -0400

    Renamed emulator to interpreter

---
+++ tests/MIPS.Assembler.Tests/InstructionTests.cs
@@ -0,0 +1,24 @@
+// Adam Dernis 2023
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace MIPS.Assembler.Tests;
+
+[TestClass]
+public class InstructionTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        string code = @"addi $t0, $s0, 100";
+
+        using var stream = new MemoryStream();
+        using var writer = new StreamWriter(stream);
+        writer.Write(code);
+        writer.Flush();
+        stream.Position = 0;
+
+        var result = Assembler.AssembleAsync(stream);
+    }
+}
