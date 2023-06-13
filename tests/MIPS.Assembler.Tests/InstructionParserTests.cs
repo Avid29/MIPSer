6b6c29632ccc88c13d4d8c4d8e37ad14998b97c4
Adam Dernis <Avid29@Live.com>
2023-06-12 21:23:51 -0400

    Instruction error logging tests

--- tests/MIPS.Assembler.Tests/InstructionParserTests.cs
+++ tests/MIPS.Assembler.Tests/InstructionParserTests.cs
@@ -1,6 +1,8 @@
 // Adam Dernis 2023
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MIPS.Assembler.Logging;
+using MIPS.Assembler.Logging.Enum;
 using MIPS.Assembler.Parsers;
 using MIPS.Models.Instructions;
 using MIPS.Models.Instructions.Enums;
@@ -17,6 +19,8 @@ public class InstructionParserTests
     private const string StoreByte = "sb $t0, -100($s0)";
     private const string Jump = "j 1000";
     private const string JumpExpression = "j 10*10";
+
+    private const string XkcdFail = "xkcd $t0, $s0, $s1";
 
     [TestMethod(Add)]
     public void AddTest()
@@ -66,16 +70,40 @@ public class InstructionParserTests
         Instruction expected = Instruction.Create(OperationCode.Jump, 100);
         RunTest(JumpExpression, expected);
     }
+    
+    [TestMethod(XkcdFail)]
+    public void XkdFailTest()
+    {
+        RunFailTest(XkcdFail, LogId.InvalidInstructionName);
+    }
 
     private static void RunTest(string input, Instruction expected)
     {
         var parser = new InstructionParser();
 
-        var nameEnd = input.IndexOf(' ');
-        var name = input[..nameEnd];
-        var args = input[(nameEnd + 1)..].Split(',');
-        var actual = parser.Parse(name, args);
+        TokenizeInstruction(input, out var name, out var args);
+        var success = parser.TryParse(name, args, out var actual);
 
+        Assert.IsTrue(success);
         Assert.AreEqual(expected, actual);
+    }
+
+    private static void RunFailTest(string input, LogId logId)
+    {
+        var logger = new AssemblerLogger();
+        var parser = new InstructionParser(null, logger);
+
+        TokenizeInstruction(input, out var name, out var args);
+        var success = parser.TryParse(name, args, out _);
+
+        Assert.IsFalse(success);
+        Assert.IsTrue(logger.Logs[0].Id == logId);
+    }
+
+    private static void TokenizeInstruction(string line, out string name, out string[] args)
+    {
+        var nameEnd = line.IndexOf(' ');
+        name = line[..nameEnd];
+        args = line[(nameEnd + 1)..].Split(',');
     }
 }
