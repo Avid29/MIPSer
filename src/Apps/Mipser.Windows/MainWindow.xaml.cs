7dc0813c3b276c02dc7805d6c9506a2ce6ecca9a
Adam Dernis <Avid29@Live.com>
2023-06-14 02:57:50 -0400

    Added template for mipser windows application

---
+++ src/Apps/Mipser.Windows/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+// Adam Dernis 2023
+
+using Microsoft.UI.Xaml;
+
+namespace Mipser.Windows;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+}
