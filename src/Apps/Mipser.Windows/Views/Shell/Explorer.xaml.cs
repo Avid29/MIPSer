082ff0364accf5738292435dc6d6d9d648d0eae2
Adam Dernis <Avid29@Live.com>
2023-06-14 09:04:28 -0400

    Added the explorer

---
+++ src/Apps/Mipser.Windows/Views/Shell/Explorer.xaml.cs
@@ -0,0 +1,19 @@
+// Adam Dernis 2023
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace Mipser.Windows.Views.Shell;
+
+/// <summary>
+/// The explorer view.
+/// </summary>
+public sealed partial class Explorer : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Explorer"/> class.
+    /// </summary>
+    public Explorer()
+    {
+        this.InitializeComponent();
+    }
+}
